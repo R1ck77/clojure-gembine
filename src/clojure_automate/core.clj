09f490982615718d47abf6d74bcc2c7034081939
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-11 16:39:46 +0200

    Add infrastructure for screen capture

--- src/clojure_automate/core.clj
+++ src/clojure_automate/core.clj
@@ -1,19 +1,28 @@
 (ns clojure-automate.core
-  (:import [java.awt Robot]
-          [java.awt.event KeyEvent])
+  (:import [java.awt Robot Rectangle Toolkit]
+           [java.awt.event KeyEvent]
+           [javax.imageio ImageIO])
   (:gen-class))
 
 (def move-delay-ms 400)
 
-(def arrows {
-             :up KeyEvent/VK_UP
+(def arrows {:up KeyEvent/VK_UP
              :down KeyEvent/VK_DOWN
              :left KeyEvent/VK_LEFT
-             :right KeyEvent/VK_RIGHT
-             })
+             :right KeyEvent/VK_RIGHT})
 
 (defn new-robot []
   (Robot.))
+
+(defn- sleep [ms]
+  (Thread/sleep ms))
+
+(defn- rectangle [ax ay bx by]
+  (Rectangle. ax ay bx by))
+
+(defn- screen-size []
+  (let [dimension (.getScreenSize (Toolkit/getDefaultToolkit))]
+    [(.width dimension) (.height dimension)]))
 
 (defn tap-key
   "Do a quick tap on the keyboard"
@@ -25,11 +34,17 @@
      (.keyRelease code))))
 
 (defn tap-character
+  "Tap the specific char"
   ([char]
    (tap-character (new-robot) char))
   ([robot char]
    (tap-key robot
             (KeyEvent/getExtendedKeyCodeForChar (int char)))))
+
+(defn tap-space
+  ([] (tap-space (new-robot)))
+  ([robot]
+   (tap-key robot KeyEvent/VK_SPACE)))
 
 (defn tap-arrow
   ([direction]
@@ -42,15 +57,19 @@
    (move (new-robot) direction))
   ([robot direction]
    (tap-arrow robot direction)
-   (Thread/sleep move-delay-ms)))
+   (sleep move-delay-ms)))
 
 
 (defn test-random-moves [delay n]
-  (Thread/sleep delay)
+  (sleep delay)
   (let [robot (new-robot)
         moves (keys arrows)]
     (repeatedly n #(move robot
                          (rand-nth moves)))))
+
+(defn take-screenshot [robot]
+  
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
