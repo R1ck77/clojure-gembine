57a5883451640077e5cdc5d1d030008293179744
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-11 15:59:38 +0200

    Add code to create virtual keypresses

--- src/clojure_automate/core.clj
+++ src/clojure_automate/core.clj
@@ -1,5 +1,38 @@
 (ns clojure-automate.core
+  (:import [java.awt Robot]
+          [java.awt.event KeyEvent])
   (:gen-class))
+
+(def arrows {
+             :up KeyEvent/VK_UP
+             :down KeyEvent/VK_DOWN
+             :left KeyEvent/VK_LEFT
+             :right KeyEvent/VK_RIGHT
+             })
+
+(defn tap-key
+  "Do a quick tap on the keyboard"
+  ([code] (tap-key (Robot.) code))
+  ([robot code]
+   (doto robot
+     (.keyPress code)
+     (.keyRelease code))))
+
+(defn tap-character
+  ([char] (tap-character (Robot.) char))
+  ([robot char]
+   (tap-key robot
+            (KeyEvent/getExtendedKeyCodeForChar (int char)))))
+
+(defn tap-arrow
+  ([arrow] (tap-arrow (Robot.) arrow))
+  ([robot arrow]
+   (tap-character robot (get arrows arrow))))
+
+(defn move-at-random []
+  )
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
