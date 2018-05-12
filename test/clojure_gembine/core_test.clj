b3c24c28e7727088676d72c7699abeb0cb577316
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-12 21:10:12 +0200

    Remove unused test file

--- test/clojure_gembine/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-automate.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-automate.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
