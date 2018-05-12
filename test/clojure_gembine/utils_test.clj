21bfd0422ef872eea81070507468ec247c7927ba
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-13 00:00:43 +0200

    Add test to cover the matching method

--- test/clojure_gembine/utils_test.clj
+++ test/clojure_gembine/utils_test.clj
@@ -1,9 +1,11 @@
 (ns clojure-gembine.utils-test
   (:require [clojure.test :refer :all]
             [clojure.java.io :refer [file input-stream resource]]
-            [clojure-gembine.utils :refer :all])
+            [clojure-gembine.utils :refer :all]
+            [clojure-gembine.parameters :refer :all])
   (:import [org.apache.commons.io IOUtils]
-           [java.io FileInputStream]))
+           [java.io FileInputStream]
+           [javax.imageio ImageIO]))
 
 (deftest test-template-as-file
   (testing "return a valid readable file"
@@ -17,3 +19,29 @@
                   result (FileInputStream. temp-file)]
         (is (IOUtils/contentEquals expected result))
         (.delete temp-file)))))
+
+(defn- load-mock-image []
+  (ImageIO/read (resource "mock/modified-with-game-over-screen.png")))
+
+(defn- in-range [[x y] [x1 y1 x2 y2]]
+  (and
+   (>= x x1) (>= y y1)
+   (<= x x2) (<= y y2)))
+
+(deftest test-match-template
+  (testing "Matching the mock with a 'game over' template returns max point in the message area"
+    (let [[x y _] (match-template (load-mock-image) game-over)]
+      (is (in-range [x y] message-area))))
+  (testing "The green triangle in the mock are is to the right below of the red one"
+    (let [mock-image (load-mock-image)
+          [xr yr _] (match-template mock-image red-triangle)
+          [xg yg _] (match-template mock-image green-triangle)]
+      (is (> xg xr))
+      (is (> yg yr))))
+  (testing "the cell [0 2] in the mock matches an empty cell better than a red bead or a green bead"
+    (let [mock-cell (get-board-cell (load-mock-image 0 2))
+          [_ _ evalue] (match-template mock-cell void-board)
+          [_ _ rvalue] (match-template mock-cell red-bead)
+          [_ _ gvalue] (match-template mock-cell green-bead)]
+      (is (> evalue rvalue))
+      (is (> evalue gvalue)))))
