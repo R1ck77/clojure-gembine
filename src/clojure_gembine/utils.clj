87ead853139bc340caa7dbfce59ff315d9eb9934
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-13 00:00:28 +0200

    Add bufimage to mat function, stub matching method

--- src/clojure_gembine/utils.clj
+++ src/clojure_gembine/utils.clj
@@ -2,7 +2,9 @@
   (:require [clojure.java.io :refer [resource input-stream]])
   (:import [org.apache.commons.io IOUtils]
            [java.io File FileOutputStream]
-           [org.opencv.highgui Highgui]))
+           [javax.imageio ImageIO]
+           [org.opencv.highgui Highgui]
+           [org.opencv.core Mat MatOfInt]))
 
 (defn load-opencv-libraries []
   (clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME))
@@ -29,3 +31,21 @@ From a cursory search, OpenCV likes files it would seem…"
   (let [temporary-file (template-as-file template-name)]
     (read-mat temporary-file)
     (.delete temporary-file)))
+
+(defn bufimage-to-mat
+  "This is a terribly slow and hacky conversion between java and OpenCV.
+
+I could do something directly with MatOfInt, but I'll worry about this if 
+it will be too slow in practice"
+  [bufimage]
+  (let [temp-file (File/createTempFile "gembine-conversion-" ".png")]
+    (ImageIO/write bufimage "png" temp-file)
+    (let [result (Highgui/imread (.getAbsolutePath temp-file))]
+      (.delete temp-file)
+      result)))
+
+(defn match-template
+  "Return the point in the bufimage that corresponds to the better match"
+  [bufimage mat]
+  
+  )
