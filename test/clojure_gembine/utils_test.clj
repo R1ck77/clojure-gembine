e8b7fe3df4d1a27dac10c071db5a11a23fda0e3d
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-12 17:22:49 +0200

    Add test to utils function

--- test/clojure_gembine/utils_test.clj
+++ test/clojure_gembine/utils_test.clj
@@ -1,12 +1,19 @@
 (ns clojure-gembine.utils-test
   (:require [clojure.test :refer :all]
-            [clojure-gembine.utils :refer :all]))
+            [clojure.java.io :refer [file input-stream resource]]
+            [clojure-gembine.utils :refer :all])
+  (:import [org.apache.commons.io IOUtils]
+           [java.io FileInputStream]))
 
 (deftest test-template-as-file
   (testing "return a valid readable file"
     (let [result (template-as-file "red-bead.png")]
-      (try
-        (is (.exists result))
-        (is (.canRead result))
-        (finally
-          (.delete result))))))
+      (is (.exists result))
+      (is (.canRead result))
+      (is (.delete result))))
+  (testing "content of the copy and of the resource are identical"
+    (let [temp-file (template-as-file "green-bead.png")]
+      (with-open [expected (input-stream (resource "templates/green-bead.png"))
+                  result (FileInputStream. temp-file)]
+        (is (IOUtils/contentEquals expected result))
+        (.delete temp-file)))))
