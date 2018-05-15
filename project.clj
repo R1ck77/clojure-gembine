e4c351b1766ef04ae46f36c3731f2c14a5a94f2f
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-15 22:38:31 +0200

    Update the project description

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject clojure-gembine "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :description "Exercise to solve Jydge's Gembine with automation"
+  :license {:name "GNU General public license v3.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [commons-io/commons-io "2.6"]
                  [opencv/opencv "3.4.1"]
-                 [opencv/opencv-native "3.4.1"]                 ]
+                 [opencv/opencv-native "3.4.1"]]
   :main ^:skip-aot clojure-gembine.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
