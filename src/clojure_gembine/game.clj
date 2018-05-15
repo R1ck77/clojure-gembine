a8400a116f6d36165975b0388fbf1d7896173e71
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-15 23:47:54 +0200

    Sketched a "move" function, with basic testing

--- src/clojure_gembine/game.clj
+++ src/clojure_gembine/game.clj
@@ -1,9 +1,55 @@
 (ns clojure-gembine.game)
 
+(def next-cell {
+                :rb :rB
+                :rB :rt
+                :rt :rs
+                :rs :rp
+                :rp :gb
+                :gb :gB
+                :gB :gt
+                :gt :gs
+                :gs :gp
+                :gp :gp}) ;;; this one is completely made up
+
 (defn rotate
-  "Rotate the board in a way that the right move corresponds to the previos up move"
+  "Rotate the board in a way that the right move corresponds to the previous up move"
   [board]
   (vec
    (map (fn [index] (vec (map #(nth % index) (reverse board))))
         [0 1 2 3])))
+
+
+(defn- tuple-conversion [a b]
+  (cond   ;;; TODO/FIXME these rule are overkill. They can be simplified…
+    ;;; same symbol on both → transmutation
+    (and (= a b) (not (nil? a))) [(get next-cell a) nil]
+    ;;; second cell empty → no movement
+    (nil? b) [a nil]
+    ;;; first empty
+    (nil? a) [b nil]
+    ;;; different symbols → no movement
+    (not= a b) [a b]
+    ;;; fail early in case of unexpected results
+    true (throw (RuntimeException. (format "Unexpected result [%s %s]" a b)))))
+
+(defn- reduce-tuple [{result :result seed :seed} value]
+  (let [[next-a next-b] (tuple-conversion seed value)]
+    {:result (conj result next-a)
+     :seed next-b}))
+
+(defn- step-row [row]
+  (let [{result :result, seed :seed} (reduce reduce-tuple
+                                             {:result []
+                                              :seed (first row)}
+                                             (rest row))]
+    (conj (vec result) seed)))
+
+(defn step
+  "Performs a left move on the board, stopping before the new element addition"
+  [board]
+  [(step-row (nth board 0))
+   (step-row (nth board 1))
+   (step-row (nth board 2))
+   (step-row (nth board 3))])
 
--- test/clojure_gembine/game_test.clj
+++ test/clojure_gembine/game_test.clj
@@ -17,3 +17,15 @@
     (is (= rotated-board (rotate demo-board))))
   (testing "4 rotations are an identity"
     (is (= demo-board (-> demo-board rotate rotate rotate rotate)))))
+
+
+(deftest test-move
+  (testing "translation only"
+    (is (= [[:rb nil nil nil]
+            [:rB :rB :rB nil]
+            [:gb nil :rt nil]
+            [:rs :gs :rs :gs]]
+           (step [[nil :rb nil nil]
+                  [:rB nil :rB :rB]
+                  [:gb nil nil :rt]
+                  [:rs :gs :rs :gs]])))))
