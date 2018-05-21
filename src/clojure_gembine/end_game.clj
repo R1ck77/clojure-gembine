3fe59c21bf851c700e900c8792142d8c0a7210ff
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-22 01:46:30 +0200

    Fix endgame procedure

--- src/clojure_gembine/end_game.clj
+++ src/clojure_gembine/end_game.clj
@@ -9,25 +9,38 @@
 
 (defn end-game-ritual [robot]
   (sleep 3000)
-  
+
+  ;;; enter secret level
   (tap-enter robot) (sleep 5000)
-  
+
+  ;;; exit to level menu
   (tap-esc robot) (sleep 2000)
 
+  ;;; select the "exit to map"
   (tap-arrow robot :right) (sleep 300)
   (tap-arrow robot :right) (sleep 300)
   (tap-arrow robot :right) (sleep 300)
   (tap-enter robot) (sleep 5000)
 
+  ;;; make sure you are on "Collectibles", then tap enter
+  (tap-arrow robot :down) (sleep 300)
+  (tap-arrow robot :down) (sleep 300)
+  (tap-arrow robot :down) (sleep 300)
+  (tap-arrow robot :left) (sleep 300)
+  (tap-arrow robot :left) (sleep 300)
+  (tap-arrow robot :left) (sleep 300)
   (tap-enter robot) (sleep 1000)
 
+  ;;; select the gem
   (tap-arrow robot :down) (sleep 300)
   (tap-arrow robot :down) (sleep 300)
   (tap-enter robot) (sleep 3000)
 
+  ;;; select gembine
   (tap-arrow robot :up ) (sleep 300)
   (tap-enter robot) (sleep 5000)
 
+  ;;; restart the gembine game to remove the tutorial
   (tap-esc robot) (sleep 300)
   (tap-arrow robot :down) (sleep 300)
   (tap-enter robot) (sleep 5000)
