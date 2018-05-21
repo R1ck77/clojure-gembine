f4ad9bce995d420dbc3f42bb10bab82545028e39
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-05-22 01:05:41 +0200

    Fix image detection (in the mocks at least)

--- src/clojure_gembine/board.clj
+++ src/clojure_gembine/board.clj
@@ -2,33 +2,46 @@
   (:require [clojure-gembine.parameters :refer :all]
             [clojure-gembine.utils :as utils]))
 
-(def symbol-to-image {:rb red-bead
-                      :rB red-ball
-                      :rt red-triangle
-                      :rs red-square
-                      :rp red-pentagon
-                      :gb green-bead
-                      :gB green-ball
-                      :gt green-triangle
-                      :gs green-square
-                      :gp green-pentagon
-                      :bb blue-bead
-                      :bB blue-ball
-                      :bt blue-triangle
-                      :bs blue-square
-                      :bp blue-pentagon})
+(def red-condition #(> (nth % 2) 80))
+(def blue-condition #(> (first %) 80))
+(def green-condition #(> (second %) 80))
 
-(def symbol-check-order [:rb :rB :rt :rs :rp
-                         :gb :gB :gt :gs :gp
-                         :bb :bB :bt :bs :bp])
+(def color-to-symbol-to-image {
+                      :red {
+                            :rb red-bead
+                            :rB red-ball
+                            :rt red-triangle
+                            :rs red-square
+                            :rp red-pentagon
+                            }
+                      :green {
+                              :gb green-bead
+                              :gB green-ball
+                              :gt green-triangle
+                              :gs green-square
+                              :gp green-pentagon
+                              }
+                      :blue {
+                             :bb green-bead
+                             :bB green-ball
+                             :bt green-triangle
+                             :bs green-square
+                             :bp green-pentagon}})
 
 (def match-threshold 0.80)
 
-(defn- score-for-symbol-in-area? [area symbol]
-  (nth (utils/match-template area (get symbol-to-image symbol)) 2))
+(defn- color-of-symbol-in-area [area]
+  (let [mat-area (utils/bufimage-to-mat area)
+        center [(/ (.rows mat-area) 2) (/ (.cols mat-area) 2)]
+        colors (seq (.get mat-area (first center) (second center)))]
+    (cond
+      (red-condition colors) :red
+      (blue-condition colors) :blue
+      (green-condition colors) :green
+      :default :black)))
 
-(defn- result-for-symbol-in-area [area symbol]
-  [symbol (score-for-symbol-in-area? area symbol)])
+(defn- score-for-template-in-area [area template]
+  (nth (utils/match-template area template) 2))
 
 (defn- remove-underscorers [match-results]
   (filter #(> (second %) match-threshold) match-results))
@@ -41,11 +54,20 @@ match-result should be like: [[:rb 0.3] [:gs 0.999] … ]"
   (when-let [filtered (remove-underscorers match-results)]
         (-> (sort-by second filtered) first first)))
 
+(defn- get-best-matching-symbol
+  [area symbols-to-image]
+  (get-first-matching-result
+   (map (fn [[symbol template]]
+          (vector symbol (score-for-template-in-area area template)))
+        symbols-to-image)))
+
 (defn get-best-match                               
   "Return a code specifying the best match"
   [area]
-  (get-first-matching-result (map (partial result-for-symbol-in-area area)
-                                  symbol-check-order)))
+  (let [matching-color (color-of-symbol-in-area area)]
+    (if (= :black matching-color)
+      nil
+      (get-best-matching-symbol area (get color-to-symbol-to-image matching-color)))))
 
 (defn- read-cell [screenshot column row]
   (let [cell-region (get-board-cell screenshot column row)]
